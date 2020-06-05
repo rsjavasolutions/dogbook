0c1620739ed99f31e5f5ee760173c11a96734f40
rsjavasolutions <stan.robert.1986@gmail.com>
2020-06-05 09:28:25 +0200

    Start project

---
+++ src/main/java/com/rsjava/doogbook/DoogbookApplication.java
@@ -0,0 +1,13 @@
+package com.rsjava.doogbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoogbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoogbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsjava/doogbook/DoogbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsjava.doogbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoogbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
